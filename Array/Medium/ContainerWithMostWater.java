e6ef5bc834e53db0574417c5f6bdac8d43b1b41a
Nantha-R <nknanthakumarcr7@gmail.com>
2022-03-11 12:06:52 +0530

    Added DSA Problem #9 & #10.

---
+++ Array/Medium/ContainerWithMostWater.java
@@ -0,0 +1,39 @@
+/*
+    Question: https://leetcode.com/problems/container-with-most-water/
+    Solution: https://www.code-recipe.com/post/container-with-most-water
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+*/
+
+public class ContainerWithMostWater {
+
+    private static int getMaximumContainer(final int[] numbers) {
+        int startingIndex = 0;
+        int endingIndex = numbers.length - 1;
+        int maximumStorageContainer= 0;
+
+        while(startingIndex!=endingIndex) {
+            final int heightOfContainer = Math.min(numbers[startingIndex], numbers[endingIndex]);
+            final int lengthOfContainer = endingIndex - startingIndex;
+            final int storageOfContainer = heightOfContainer * lengthOfContainer;
+
+            maximumStorageContainer = Math.max(storageOfContainer, maximumStorageContainer);
+
+            if(numbers[startingIndex] < numbers[endingIndex]) {
+                startingIndex += 1;
+            } else {
+                endingIndex -= 1;
+            }
+        }
+
+        return maximumStorageContainer;
+    }
+
+    public static void main(String[] args) {
+        final int[] numbers = {1,8,6,2,5,4,8,3,7};
+        if (numbers.length <= 1) {
+            System.out.println("No QUANTITY OF WATER CAN BE STORED.");
+        }
+        System.out.println(getMaximumContainer(numbers));
+    }
+}
---
+++ Array/Medium/TripletsSum.java
@@ -0,0 +1,49 @@
+/*
+    Question: https://leetcode.com/problems/3sum/submissions/
+    Solution: https://www.code-recipe.com/post/three-sum
+    Time Complexity: O(n^2)
+    Space Complexity: O(n)
+*/
+
+public class TripletsSum {
+
+    private static HashSet<ArrayList<Integer>> getTriplets(final int[] numbers) {
+        // Triplets needs to be unique, Hence using HashSet here.
+        final HashSet<ArrayList<Integer>> triplets = new HashSet<>();
+
+        Arrays.sort(numbers);
+        for(int i=0;i<numbers.length;i++) {
+            final int firstElement = numbers[i];
+            // Since FirstElement + SecondElement +ThirdElement = 0, If FirstElement is positive then
+            // second and third elements are also positive and their sum will be greater than 0.
+            if(firstElement > 0) {
+                break;
+            }
+            final HashMap<Integer, Boolean> elements = new HashMap();
+
+            for (int j=i+1;j<numbers.length;j++) {
+                final int secondElement = numbers[j];
+                final int requiredThirdElement = - firstElement - secondElement;
+                if(elements.get(requiredThirdElement) != null) {
+                    final ArrayList<Integer> triplet = new ArrayList<Integer>();
+                    Collections.addAll(triplet, firstElement, secondElement, requiredThirdElement);
+                    triplet.sort(Comparator.naturalOrder());
+                    triplets.add(triplet);
+                }
+                elements.put(secondElement, Boolean.TRUE);
+            }
+        }
+        return triplets;
+    }
+
+    public static void main(String[] args) {
+        final int[] numbers = {-1,0,1,2,-1,-4};
+        if (numbers.length <= 2) {
+            System.out.println("NO TRIPLETS FOUND.");
+        }
+        final Set<ArrayList<Integer>> triplets =  getTriplets(numbers);
+        for(final ArrayList<Integer> triplet : triplets) {
+            System.out.println(triplet.toString());
+        }
+    }
+}
