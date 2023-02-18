ad9829831128f9472a7595400e8ec995c3e7f0aa
Nantha kumar <rvnanthakumar@gmail.com>
2023-02-18 20:02:30 +0530

    Added DSA Problem #11 & #12.

---
+++ Binary/Easy/FindNumberOfOccurenceOfOnesInBinary.java
@@ -0,0 +1,24 @@
+/*
+    Question: https://leetcode.com/problems/number-of-1-bits/solutions/
+    Solution: https://leetcode.com/problems/number-of-1-bits/solutions/3186879/java-easy-and-explained-solution-speed-beats-100/
+    Time Complexity: O(n) where n is the number of binary digits in a number.
+    Space Complexity: O(1)
+*/
+public class FindNumberOfOccurenceOfOnesInBinary {
+
+    private static int getNumberOfOccurences(int binaryNumber) {
+        int numberOfOccurences = 0;
+        while (binaryNumber != 0) {
+            if ((binaryNumber & 1) == 1) {
+                numberOfOccurences += 1;
+            }
+            binaryNumber = binaryNumber >>> 1;
+        }
+        return numberOfOccurences;
+    }
+
+    public static void main(String[] args) {
+        final int binaryNumber = 7;
+        System.out.printf("Number of 1 digits = %d", getNumberOfOccurences(binaryNumber));
+    }
+}
---
+++ Binary/Easy/SumOfTwoIntegersWithoutUsingAdditionOperator.java
@@ -0,0 +1,24 @@
+/*
+    Question: https://leetcode.com/problems/sum-of-two-integers/
+    Solution: https://leetcode.com/problems/sum-of-two-integers/solutions/132479/simple-explanation-on-how-to-arrive-at-the-solution/
+    Time Complexity: O(1)
+    Space Complexity: O(1)
+*/
+
+public class SumOfTwoIntegersWithoutUsingAdditionOperator {
+
+  private static int getSum(int integerOne, int integerTwo) {
+    while(integerTwo != 0) {
+      final int carry = integerOne & integerTwo;
+      integerOne = integerOne ^ integerTwo;
+      integerTwo = carry << 1;
+    }
+    return integerOne;
+  }
+
+  public static void main(String[] args) {
+    final int integerOne = 3;
+    final int integerTwo = 7;
+    System.out.printf("SUM = %d", getSum(integerOne,integerTwo));
+  }
+}
